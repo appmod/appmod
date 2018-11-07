e6710b9bf38a1117f706160b14e6ed9e7c55ad3d
zhiyuan <wanzhiyuan@gmail.com>
2018-11-07 20:28:40 +0800

    update images for receive anomaly, take own action, and receive advice

--- app/Adviser/app/src/main/java/com/smu/appmod/OwnActionActivity.java
+++ app/Adviser/app/src/main/java/com/smu/appmod/OwnActionActivity.java
@@ -73,7 +73,7 @@ public class OwnActionActivity extends Activity {
         text.setText(Html.fromHtml(str));
 
         image = (ImageView) findViewById(R.id.app);
-        String appname = message.split(" ")[0].trim();
+        String appname = message.split(" ")[0].trim(); 
         if (appname.equalsIgnoreCase("YouTube")) {
             image.setImageResource(R.drawable.app_yt);
         } else if (appname.equalsIgnoreCase("Facebook")) {
@@ -90,6 +90,22 @@ public class OwnActionActivity extends Activity {
             image.setImageResource(R.drawable.app_cr);
         } else if (appname.equalsIgnoreCase("Sudoku")) {
             image.setImageResource(R.drawable.app_sk);
+        } else if (appname.equalsIgnoreCase("Bubble")) {
+            image.setImageResource(R.drawable.app_bubble);
+        } else if (appname.equalsIgnoreCase("Compass")) {
+            image.setImageResource(R.drawable.app_compass);
+        } else if (appname.equalsIgnoreCase("Linkedin")) {
+            image.setImageResource(R.drawable.app_linkedin);
+        } else if (appname.equalsIgnoreCase("Microsoft")) {
+            image.setImageResource(R.drawable.app_outlook);
+        } else if (appname.equalsIgnoreCase("Tumblr")) {
+            image.setImageResource(R.drawable.app_tumblr);
+        } else if (appname.equalsIgnoreCase("Mahjong")) {
+            image.setImageResource(R.drawable.app_mahjong);
+        } else if (appname.equalsIgnoreCase("Line")) {
+            image.setImageResource(R.drawable.app_line);
+        } else if (appname.equalsIgnoreCase("VLC")) {
+            image.setImageResource(R.drawable.app_vlc);
         } else {
             image.setImageResource(R.drawable.app);
         }
