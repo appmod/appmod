db95c456afcdada83017c2c578e08399d6b04cfe
zhiyuan <wanzhiyuan@gmail.com>
2018-11-13 17:23:23 +0800

    Disable 'seek advice' for advisee

--- app/Adviser/app/src/main/java/com/smu/appmod/DialogActivity.java
+++ app/Adviser/app/src/main/java/com/smu/appmod/DialogActivity.java
@@ -3,11 +3,16 @@ package com.smu.appmod;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class DialogActivity extends Activity implements OnClickListener {
 
@@ -16,9 +21,19 @@ public class DialogActivity extends Activity implements OnClickListener {
     TextView tv;
     UtilityClass utility;
     static String adviserName;
+    static Set<String> ownActionSet = new HashSet<String>();
+    static String[] ownactionStrs = null;
 
+    private static final String TAG = "DA";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (ownactionStrs == null) {
+            ownactionStrs = getResources().getStringArray(R.array.ownaction);
+            for (String str : ownactionStrs) {
+                ownActionSet.add(str);
+            }
+        }
+
         super.onCreate(savedInstanceState);
         getWindow().setTitle("Anomaly");
         setContentView(R.layout.dialogactivity);
@@ -41,10 +56,36 @@ public class DialogActivity extends Activity implements OnClickListener {
                 utility = new UtilityClass(getApplicationContext());
                 adviserName = utility.getAdviserName();
                 if (!adviserName.equals("notset")) {
-                    Intent intt = new Intent(getApplicationContext(), GetAdvice.class);
+                    Intent intt = null;
+                    if (ownActionSet.contains(message)) {
+                        intt = new Intent(getApplicationContext(), OwnActionActivity.class);
+                    } else {
+                        intt = new Intent(getApplicationContext(), GetAdvice.class);
+                    }
                     intt.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     intt.putExtra("date", date);
                     intt.putExtra("message", message);
+
+                    try {
+                        final UtilityClass utility = new UtilityClass(getApplicationContext());
+                        final String urlString = "http://flyer.sis.smu.edu.sg/AdviseProject/getAnomalyID.php";
+                        if (utility.hasActiveInternetConnection("http://www.google.com")) {
+                            String notification = message.split("Click")[0].trim();
+                            String anomalyid = utility.serverInteraction(urlString, notification);
+                            intt.putExtra("anomalyid", anomalyid);
+                        } else {
+                            DialogActivity.this.runOnUiThread(new Runnable() {
+                                public void run() {
+                                    Toast toast = Toast.makeText(DialogActivity.this, "Please check internet connectivity and try again!", Toast.LENGTH_SHORT);
+                                    toast.show();
+                                }
+                            });
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "***** Error=" + e.toString());
+                        return;
+                    }
+
                     startActivity(intt);
                     this.finish();
                     break;
--- app/Adviser/app/src/main/java/com/smu/appmod/GetAdvice.java
+++ app/Adviser/app/src/main/java/com/smu/appmod/GetAdvice.java
@@ -1,6 +1,7 @@
 package com.smu.appmod;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
@@ -11,6 +12,9 @@ import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,20 +23,26 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import android.widget.ImageView;
 
 public class GetAdvice extends Activity {
-    Button getadvice_btn, ownaction_btn;
+    Button getadvice_btn, ownaction_btn, detailBtn, closeBtn, doNothing, uninstall, kill;
+    static String[] detail = null;
+    static Map anomaly2detail = new HashMap<String, String[]>();
     TextView text;
     UtilityClass utility;
     static String phone, date;
     ProgressDialog progressDialog;
-    static String adviserName, message;
+    static String adviserName, message, anomalyid;
     ImageView image;
     private static final String TAG = "GA";
+
+    Dialog dialog, dialog1;
 
     static Set<String> ownActionSet = new HashSet<String>();
     static String[] ownactionStrs = null;
@@ -46,7 +56,24 @@ public class GetAdvice extends Activity {
     }
 
     @Override
+    public void onPause(){
+        super.onPause();
+        if (dialog != null) {
+            dialog.dismiss();
+            dialog = null;
+        }
+        if (dialog1 != null) {
+            dialog1.dismiss();
+            dialog1  = null;
+        }
+    }
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
+        Intent intent = getIntent();
+        date = intent.getStringExtra("date");
+        message = intent.getStringExtra("message");
+        //Log.e(TAG, "onCreate" + message);
         if (ownactionStrs == null) {
             ownactionStrs = getResources().getStringArray(R.array.ownaction);
             for (String str : ownactionStrs) {
@@ -54,45 +81,221 @@ public class GetAdvice extends Activity {
             }
         }
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.getadvice);
-        getActionBar().setIcon(R.drawable.icon);
-        getActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#08457E")));
-        getActionBar().setTitle(Html.fromHtml("<font color='#FFFFFF'>Anomaly</font>"));
-        utility = new UtilityClass(getApplicationContext());
-        adviserName = utility.getAdviserName();
-        Intent intent = getIntent();
-        date = intent.getStringExtra("date");
-        message = intent.getStringExtra("message");
-        image = (ImageView) findViewById(R.id.app);
-        String appname = message.split(" ")[0].trim();
-        Helper.setAppImage(image, appname);
-        text = (TextView) findViewById(R.id.tv);
-        String str = "<font color=#08457E>Anomaly - </font><font color=#CC0000>" + message.split("Click")[0].trim() + "</font><font color=#08457E><br><br>This may be a suspicious activity. You can take advice from your Advisor: </font><font color=#CC0000>" + adviserName.trim() + "</font>" +
-                "<font color=#08457E> by clicking on the button below.</font> ";
-        text.setText(Html.fromHtml(str));
-        getadvice_btn = (Button) findViewById(R.id.getadvice);
 
         if (ownActionSet.contains(message)) {
-            getadvice_btn.setVisibility(View.GONE);
+            setContentView(R.layout.takeactiondialog);
+            getActionBar().setIcon(R.drawable.icon);
+            getActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#08457E")));
+            getActionBar().setTitle(Html.fromHtml("<font color='#FFFFFF'>Take Action</font>"));
+            try {
+                final UtilityClass utility = new UtilityClass(getApplicationContext());
+                final String urlString = "http://flyer.sis.smu.edu.sg/AdviseProject/getAnomalyID.php";
+                if (utility.hasActiveInternetConnection("http://www.google.com")) {
+                    String notification = message.split("Click")[0].trim();
+                    anomalyid = utility.serverInteraction(urlString, notification);
+                } else {
+                    GetAdvice.this.runOnUiThread(new Runnable() {
+                        public void run() {
+                            Toast toast = Toast.makeText(GetAdvice.this, "Please check internet connectivity and try again!", Toast.LENGTH_SHORT);
+                            toast.show();
+                        }
+                    });
+                }
+            } catch (IOException e) {
+                Log.e(TAG, "***** Error=" + e.toString());
+                return;
+            }
+            text = (TextView) findViewById(R.id.text);
+            String str = "<font color=#08457E>Anomaly - </font><font color=#CC0000>" + message.split("Click")[0].trim() + "</font><font color=#08457E><br><br>You have chosen to take your own action.</font>";
+            text.setText(Html.fromHtml(str));
+
+            image = (ImageView) findViewById(R.id.app);
+            String appname = message.split(" ")[0].trim();
+            Helper.setAppImage(image, appname);
+            detailBtn = (Button) findViewById(R.id.detail_adviee_btn);
+
+            detailBtn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (detail == null) {
+                        detail = getResources().getStringArray(R.array.detail);
+                        for (String detail_item : detail) {
+                            String[] detail_array = detail_item.split(",");
+                            String[] id_context = {detail_array[1],detail_array[2]};
+                            anomaly2detail.put(detail_array[0],id_context);
+                        }
+                    }
+
+                    String[] id_context = (String[]) anomaly2detail.get(message);
+
+
+                    dialog = new Dialog(GetAdvice.this);
+                    dialog.setContentView(R.layout.detaildialog);
+                    dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+                    dialog.setTitle(Html.fromHtml("<font color='#08457E'><b>Details of the Anomaly</font>"));
+
+                    if (id_context != null) {
+                        WebView webviewAbout = (WebView) dialog.findViewById(R.id.google_play);
+                        webviewAbout.setWebViewClient(new WebViewClient());
+                        //webviewAbout.loadUrl("https://play.google.com/store/apps/details?id=" + id_context[0] + "&hl=en");
+                        webviewAbout.loadUrl("file:///android_asset/" + id_context[0]);
+
+                        WebView webviewContext = (WebView) dialog.findViewById(R.id.context);
+                        webviewContext.setWebViewClient(new WebViewClient());
+                        webviewContext.loadUrl("file:///android_asset/" + id_context[1]);
+                    }
+
+
+                    closeBtn = (Button) dialog.findViewById(R.id.close_btn);
+                    if (closeBtn != null) {
+                        closeBtn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                dialog.dismiss();
+                            }
+                        });
+                    }
+
+                    if (dialog != null && !dialog.isShowing())
+                        dialog.show();
+                }
+            });
+
+            final DBManager dbManager;
+            dbManager = new DBManager(GetAdvice.this);
+            dbManager.open();
+            doNothing = (Button) findViewById(R.id.dialogdonothing);
+            uninstall = (Button) findViewById(R.id.dialogButtonuninstall);
+            kill = (Button) findViewById(R.id.dialogButtonkill);
+
+
+            doNothing.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    progressDialog = ProgressDialog.show(GetAdvice.this, "", "Please wait...", true);
+                    dbManager.updateReadDepndnt(date);
+                    new GetAdvice.SendDetailsOwnAction("Do nothing", anomalyid.trim()).execute();
+                }
+            });
+            uninstall.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    progressDialog = ProgressDialog.show(GetAdvice.this, "", "Please wait...", true);
+                    dbManager.updateReadDepndnt(date);
+                    new GetAdvice.SendDetailsOwnAction("Uninstall", anomalyid.trim()).execute();
+                }
+            });
+            kill.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    progressDialog = ProgressDialog.show(GetAdvice.this, "", "Please wait...", true);
+                    dbManager.updateReadDepndnt(date);
+                    new GetAdvice.SendDetailsOwnAction("Kill", anomalyid.trim()).execute();
+                }
+            });
+        } else {
+            setContentView(R.layout.getadvice);
+            getActionBar().setIcon(R.drawable.icon);
+            getActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#08457E")));
+            getActionBar().setTitle(Html.fromHtml("<font color='#FFFFFF'>Anomaly</font>"));
+            utility = new UtilityClass(getApplicationContext());
+            adviserName = utility.getAdviserName();
+            image = (ImageView) findViewById(R.id.app);
+            String appname = message.split(" ")[0].trim();
+            Helper.setAppImage(image, appname);
+            text = (TextView) findViewById(R.id.tv);
+            String str = "<font color=#08457E>Anomaly - </font><font color=#CC0000>" + message.split("Click")[0].trim() + "</font><font color=#08457E><br><br>This may be a suspicious activity. You can take advice from your Advisor: </font><font color=#CC0000>" + adviserName.trim() + "</font>" +
+                    "<font color=#08457E> by clicking on the button below.</font> ";
+            text.setText(Html.fromHtml(str));
+            getadvice_btn = (Button) findViewById(R.id.getadvice);
+
+
+            ownaction_btn = (Button) findViewById(R.id.ownaction);
+            getadvice_btn.setOnClickListener(new View.OnClickListener() {
+                                                 public void onClick(View view) {
+                                                     progressDialog = ProgressDialog.show(GetAdvice.this, "", "Please wait...", true);
+                                                     new GetIDAsyncTask("yes").execute();
+                                                 }
+                                             }
+            );
+            ownaction_btn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    progressDialog = ProgressDialog.show(GetAdvice.this, "", "Please wait...", true);
+                    new GetIDAsyncTask("no").execute();
+                }
+            });
+        }
+        super.onCreate(savedInstanceState);
+    }
+    class SendDetailsOwnAction extends AsyncTask<String, Void, Boolean> {
+        private String mResponse;
+        private String mAnomalyid;
+
+        public SendDetailsOwnAction( String response, String anomalyid) {
+            mResponse = response;
+            mAnomalyid = anomalyid;
         }
 
-        ownaction_btn = (Button) findViewById(R.id.ownaction);
-        getadvice_btn.setOnClickListener(new View.OnClickListener() {
-                                             public void onClick(View view) {
-                                                 progressDialog = ProgressDialog.show(GetAdvice.this, "", "Please wait...", true);
-                                                 new GetIDAsyncTask("yes").execute();
-                                             }
-                                         }
-        );
-        ownaction_btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                progressDialog = ProgressDialog.show(GetAdvice.this, "", "Please wait...", true);
-                new GetIDAsyncTask("no").execute();
-            }
-        });
-    }
 
+        @Override
+        protected Boolean doInBackground(String... params) {
+            try {
+                final UtilityClass utility = new UtilityClass(getApplicationContext());
+                final String urlString = "http://flyer.sis.smu.edu.sg/AdviseProject/ownaction.php";
+                if (utility.hasActiveInternetConnection("http://www.google.com")) {
+                    String phone = utility.getLoginPhone();
+                    utility.serverInteraction(urlString, phone + ":" + mAnomalyid + ":" + message + ":" + mResponse + ":" + "WithoutTakingAdvice");
+                } else {
+                    GetAdvice.this.runOnUiThread(new Runnable() {
+                        public void run() {
+                            Toast toast = Toast.makeText(GetAdvice.this, "Please check internet connectivity and try again!", Toast.LENGTH_SHORT);
+                            toast.show();
+                        }
+                    });
+                }
+            } catch (IOException e) {
+                Log.e(TAG, "***** Error=" + e.toString());
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            dialog1 = new Dialog(GetAdvice.this);
+            dialog1.setContentView(R.layout.actioncompleteddialog);
+            dialog1.setTitle(Html.fromHtml("<font color='#08457E'><b>Action Completed!</font>"));
+            Button okbtn = (Button) dialog1.findViewById(R.id.ok);
+            okbtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(GetAdvice.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+            });
+            TextView replyTV = (TextView) dialog1.findViewById(R.id.reply);
+            String text = null;
+            if (mResponse.equals("Do nothing")) {
+                text = "Nothing has been done to the application.";
+            } else if (mResponse.equals("Uninstall")) {
+                text = "Please uninstall the application manually.";
+            } else if (mResponse.equals("Kill")) {
+                text = "Please kill the application manually.";
+            }
+            replyTV.setText(text);
+            if (dialog1 != null && !dialog1.isShowing())
+                dialog1.show();
+
+            DBManager dbManager = new DBManager(GetAdvice.this);
+            dbManager.open();
+            //String query2 = "INSERT INTO ADVICES(_id,date,seeker_name,anomaly,advice,followed,pending) values ('" + "depen" + "','"
+            //        + date + "','" + "myself" + "','" + message + "','" + "You did not take advice for this anomaly." + "','" + "actiontaken" + "','" + "no" + "')";
+            String query2 = "INSERT INTO ADVICES(_id,date,seeker_name,anomaly,advice,followed,pending) values ('" + "depen" + "','"
+                    + date + "','" + "myself" + "','" + message + "','" + "You took your own action for this anomaly." + "','" + text + "','" + "no" + "')";
+            dbManager.executeQuery(query2);
+            dbManager.updateAnomaly(date.trim(), mAnomalyid);
+        }
+    }
     class SendDetailsAsyncTask extends AsyncTask<String, Void, Boolean> {
         private String mAnomalyid;
         String flag;
